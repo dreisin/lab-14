f2245da0e6798929d99c367a93315e6b0e6d3dd6
dreisin <40753019+dreisin@users.noreply.github.com>
2020-05-30 11:05:35 +0300

    Add files via upload

---
+++ ExprCalc.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace lab14
+{
+    class ExprCalc
+    {
+        // returns rpn of expression
+        public static IEnumerable<char> GetRPN(IEnumerable<char> expr)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var token in expr)
+            {
+                if (token == ')')
+                {
+                    stack.Pop();//gets open bracket
+                }
+                else
+                if (!char.IsLetter(token))
+                {
+                    stack.Push(token);//puts operation in stack
+                    continue;
+                }
+                else
+                    yield return token;//returns variable
+
+                //gets everything from stack (except brackets)
+                while (stack.Count > 0 && stack.Peek() != '(')
+                    yield return stack.Pop();
+            }
+        }
+
+        //counts expression in rpn
+        public static bool Calculate(IEnumerable<char> rpn, Dictionary<char, int> variableValue)
+        {
+            var stack = new Stack<bool>();
+
+            foreach (var token in rpn)
+                switch (token)
+                {
+                    case '¬': stack.Push(!stack.Pop()); break;
+                    case '∨': stack.Push(stack.Pop() | stack.Pop()); break;
+                    case '∧': stack.Push(stack.Pop() & stack.Pop()); break;
+                    case '→': stack.Push(stack.Pop() | !stack.Pop()); break;
+                    case '⇿': stack.Push(stack.Pop() == stack.Pop()); break;
+                    case '⊕': stack.Push(stack.Pop() != stack.Pop()); break;
+                    case '↓': stack.Push(!stack.Pop() & !stack.Pop()); break;
+                    case '|': stack.Push(!(stack.Pop() & stack.Pop())); break;
+                    case '0': stack.Push(false); break;
+                    case '1': stack.Push(true); break;
+                    default: stack.Push(variableValue[token] == 1); break;
+                }
+
+            return stack.Pop() ? true : false;
+        }
+
+        //gets all combinations of variables
+        public static IEnumerable GetAllCombinations(IList<char> variables, int index, Dictionary<char, int> varValues)
+        {
+            if (index >= variables.Count) yield return null;
+            else
+                foreach (var val in Enumerable.Range(0, 2))
+                {
+                    varValues[variables[index]] = val;
+                    foreach (var temp in GetAllCombinations(variables, index + 1, varValues))
+                        yield return temp;
+                }
+        }
+
+        public static IList<char> GetVariables(string expression)
+        {
+            try
+            {
+                return Regex.Replace(expression, @"[¬,∨,∧,→,⇿,⊕,↓,|,0,1,(,)]", "").ToArray().Distinct().ToArray();
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+
+namespace lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expr;
+            try
+            {             
+                StreamReader file = new StreamReader(args[0]);
+                StreamWriter outfile = new StreamWriter(args[1]);
+
+                while ((expr = file.ReadLine()) != null)
+                {
+                    //getting rpn
+                    var rpn = ExprCalc.GetRPN(expr).ToArray();
+
+                    Console.WriteLine("rpn:");
+                    Console.WriteLine(rpn);
+
+                    outfile.WriteLine("rpn:");
+                    outfile.WriteLine(rpn);
+
+                    LinkedList<String> sdnf = new LinkedList<string>();
+                    LinkedList<String> sknf = new LinkedList<string>();
+                    var varValues = new Dictionary<char, int>();
+                    var headerShown = false;
+
+                    Console.WriteLine("truth table:");
+                    foreach (var combination in ExprCalc.GetAllCombinations(ExprCalc.GetVariables(expr), 0, varValues))
+                    {
+                        //calculating expression
+                        var res = ExprCalc.Calculate(rpn, varValues);
+
+                        string str = "";
+
+                        if (res)//sdnf
+                        {
+                            foreach (var val in varValues)
+                            {
+                                if (val.Value == 0) str += "¬" + val.Key + " + ";
+
+                                else str += val.Key + " + ";
+                            }
+                            if (str.EndsWith(' '))
+                            {
+                                str = str.Substring(0, str.Length - 3);
+                            }
+                            sdnf.AddLast(str);
+                        }
+                        else//sknf
+                        {
+                            foreach (var val in varValues)
+                            {
+                                if (val.Value == 1) str += "¬" + val.Key + " * ";
+
+                                else str += val.Key + " * ";
+                            }
+                            if (str.EndsWith(' '))
+                            {
+                                str = str.Substring(0, str.Length - 3);
+                            }
+                            sknf.AddLast(str);
+                        }
+
+                        //showing header
+                        if (!headerShown)
+                        {
+                            foreach (var var in varValues.Keys)
+                            {
+                                Console.Write(var + "\t");
+                                outfile.Write(var + "\t");
+                            }
+                            Console.WriteLine(expr);
+                            outfile.WriteLine(expr);
+                            headerShown = true;
+                        }
+
+                        //showing row
+                        foreach (var var in varValues.Values)
+                        {
+                            Console.Write(var + "\t");
+                            outfile.Write(var + "\t");
+                        }
+                        if (res)
+                        {
+                            Console.WriteLine('1');
+                            outfile.WriteLine('1');
+                        }
+                        else
+                        {
+                            Console.WriteLine('0');
+                            outfile.WriteLine('0');
+                        }
+                    }
+
+                    //output of sdnf
+                    Console.WriteLine("sdnf:");
+                    outfile.WriteLine("sdnf:");
+
+                    uint cnt = 0;
+                    foreach (var val in sdnf)
+                    {
+                        if (cnt++ == 0)
+                        {
+                            Console.Write("({0})", val);
+                            outfile.Write("({0})", val);
+                        }
+                        else
+                        {
+                            Console.Write(" + ({0})", val);
+                            outfile.Write(" + ({0})", val);
+                        }
+                    }
+                    Console.Write(" = 1");
+                    outfile.Write(" = 1");
+
+                    Console.WriteLine();
+                    outfile.WriteLine();
+
+                    //output of sknf
+                    Console.WriteLine("sknf: ");
+                    outfile.WriteLine("sknf: ");
+
+                    cnt = 0;
+                    foreach (var val in sknf)
+                    {
+                        if (cnt++ == 0)
+                        {
+                            Console.Write("({0})", val);
+                            outfile.Write("({0})", val);
+                        }
+                        else
+                        {
+                            Console.Write(" * ({0})", val);
+                            outfile.Write(" * ({0})", val);
+                        }
+                    }
+                    Console.Write(" = 0\n\n");
+                    outfile.Write(" = 0\n\n");
+                }
+                file.Close();
+                outfile.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
